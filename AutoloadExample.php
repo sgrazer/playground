e9ac3084f85386d1cc6c62cb9bc5fde9251541de
sgrazer <grazer@grazersoft.com>
2015-03-18 02:33:21 +0100

    xxxx

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace grazersoft\gradminpanel;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
